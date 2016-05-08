c63947cb08ffc142cd4e23063f87fdbe20c97b09
zFn4Fun <zfn4fun@yahoo.com>
2016-05-08 20:46:10 +0300

    removed quotes that were bigger than 130 chars in length

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 (function() {
     var quotes = [{
-        quote: "Remember one thing. Through every dark night, there's a bright day after that. So no matter how hard it get, stick your chest out. Keep your head up, and handle it.",
-        author: "Tupac",
-        imgSrc: "tupac.jpg"
-    }, {
         quote: "Never looking back or too far in front of me. The present is a gift and I just wanna BE.",
         author: "Common",
         imgSrc: "common.jpg"
@@ -19,10 +15,6 @@
         quote: "If you admire somebody you should go on 'head tell 'em. People never get the flowers while they can still smell 'em.",
         author: "Kanye West",
         imgSrc: "kanyewest.jpg"
-    }, {
-        quote: "Everything will eventually come to an end. So try to savor the moment, cause time flies, don't it? The beauty of life, you gotta make it last for the better. Cuz nothin' lasts forever.",
-        author: "Nas",
-        imgSrc: "nas.jpg"
     }, {
         quote: "If you're scared to take chances, you'll never have the answers.",
         author: "Nas",
@@ -48,10 +40,6 @@
         author: "Rakim",
         imgSrc: "rakim.jpg"
     }, {
-        quote: "Sometimes you have the trends that's not that cool. You may have certain artists portraying these trends and don't really have that lifestyle, and then it gives off the wrong thing. And it becomes kinda corny after awhile. It's really about keeping hip-hop original and pushing away the corniness in it.",
-        author: "Kendrick Lamar",
-        imgSrc: "kdot.jpg"
-    }, {
         quote: "Shit don't change until you get up and wash yo' ass.",
         author: "Kendrick Lamar",
         imgSrc: "kdot.jpg"
@@ -60,25 +48,9 @@
         author: "The Underachievers",
         imgSrc: "underachievers.jpg"
     }, {
-        quote: "A lot of times, when people say hip hop, they don't know what they're talking about. They just think of the rappers. When you talk about hip hop, you're talking about the whole culture and movement. You have to take the whole culture for what it is.",
-        author: "Afrika Bambaataa",
-        imgSrc: "bambaataa.jpg"
-    }, {
-        quote: "What worries me are these so-called radio stations with program directors who don't play all the different flavors of hip hop. They should play the old with the new, 24/7, 365 days a year. A lot of these program directors are just jiving around and not playing all the good music for the people.",
-        author: "Afrika Bambaataa",
-        imgSrc: "bambaataa.jpg"
-    }, {
-        quote: "You can spend minutes, hours, days, weeks, or even months over-analyzing a situation, trying to put the pieces together, justifying what could've, would've happened... or you can just leave the pieces on the floor and move the fuck on.",
-        author: "Tupac",
-        imgSrc: "tupac.jpg"
-    }, {
         quote: "Life is what we make it and a chance is like a picture, it'd be nice if you'd just take it.",
         author: "Drake",
         imgSrc: "drake.jpg"
-    }, {
-        quote: "Wherever I go, I bring the culture with me, so that they can understand that it's attainable. I didn't do it any other way than through Hip Hop.",
-        author: "Jay-Z",
-        imgSrc: "jayz.jpeg"
     }, {
         quote: "Create a dream, and then get off your ass and then chase it.",
         author: "Hopsin",
