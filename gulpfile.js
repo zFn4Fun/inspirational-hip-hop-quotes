4f40bbd8faa228b9affae5afe25e37c21a1c31c1
zFn4Fun <zfn4fun@yahoo.com>
2016-05-23 11:50:59 +0300

    updated tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,48 @@
 const gulp = require('gulp');
+const del = require('del');
 const babel = require("gulp-babel");
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('babel', () => {
+gulp.task('clean', () => {
+    del(['dist/*',]);
+});
+
+gulp.task('compressjs', () => {
     gulp.src('src/*.js')
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('uglify', () => {
-    gulp.src('src/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('htmlmin', () => {
+gulp.task('compresshtml', () => {
     gulp.src('src/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('cssnano', () => {
+gulp.task('compresscss', () => {
     gulp.src('src/*.css')
         .pipe(cssnano())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('imagemin', () => {
+gulp.task('compressimg', () => {
     gulp.src('src/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('watch', ['babel'], () => {
-    gulp.watch("src/*.js", ['babel']);
+gulp.task('default', ['clean', 'compressjs', 'compresshtml', 'compresscss',
+    'compressimg']);
+
+gulp.task('watch', ['default'], () => {
+    gulp.watch('src/*.js', ['compressjs']);
+    gulp.watch('src/*.html', ['compresshtml']);
+    gulp.watch('src/*.css', ['compresscss']);
+    gulp.watch('src/img/*', ['compressimg']);
 });
